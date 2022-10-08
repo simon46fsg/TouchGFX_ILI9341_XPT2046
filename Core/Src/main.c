52f0dc58ad1fdca2fd2a920bc49976308f373a9d
simon <simon46fsg@gmail.com>
2022-10-08 23:40:48 +0200

    Touch works but display flickers

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,6 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "../../Display_drivers/ili9341.h"
+#include "../../Display_drivers/xpt2046.h"
 
 /* USER CODE END Includes */
 
@@ -108,6 +109,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
   //init display
   ILI9341_Init();
+  XPT2046_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -279,11 +281,11 @@ static void MX_SPI2_Init(void)
   hspi2.Instance = SPI2;
   hspi2.Init.Mode = SPI_MODE_MASTER;
   hspi2.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi2.Init.DataSize = SPI_DATASIZE_4BIT;
+  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
   hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi2.Init.NSS = SPI_NSS_SOFT;
-  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
   hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
